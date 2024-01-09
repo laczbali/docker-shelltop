798d6f197ed7531bbece67cfb03aed068190c5bf
Balazs Laczko <laczbali@gmail.com>
2024-01-09 16:01:28 +0100

    experimenting with dev env setup

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using DockerWrapper;
+
+Console.WriteLine("Ver:" + Class1.GetVersion());
+Console.WriteLine(Class1.ListContainers());
+
+while(true)
+{
+    Thread.Sleep(1000);
+    Console.WriteLine("Hello World!");
+}
